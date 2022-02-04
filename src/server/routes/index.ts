ce45082c4907b496fc3e04aee5bd725fce8bf01f
saqibs98 <ssaqib@ssidecisions.com>
2022-02-04 21:38:09 +0500

    conversion to ts except migrations, controllers, models

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,6 +1,6 @@
 const userController = require("../controllers");
 
-module.exports = (app) => {
+module.exports = (app:any) => {
   app.get("/users", userController.getAllUsers);
   app.post("/login", userController.findUser);
   app.get("/messages", userController.getAllMessages);
--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,12 +1,12 @@
-function chat(io) {
-  io.on("connection", (socket) => {
+function chat(io:any) {
+  io.on("connection", (socket:any) => {
     console.log("user connected", socket.id);
 
     io.emit("welcome", "Welcome to my app!");
 
-    socket.on("addUser", (user_id) => {
-      console.log(data);
-      socket.broadcast.emit("message", data);
+    socket.on("addUser", (user_id:any) => {
+      //console.log(data);
+      socket.broadcast.emit("message", user_id);
     });
 
     socket.on("disconnect", () => {
